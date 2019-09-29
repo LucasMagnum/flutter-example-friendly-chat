a371675025c25ca02fd770af71a620887595f464
Lucas Magnum <lucasmagnum2@gmail.com>
2019-09-29 17:26:58 -0300

    Add text input and send message button

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FriendlyChatApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Friendly chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-            title: Text('Friendly chat')
+      title: "Friendly chat",
+      home: ChatScreen(),
+    );
+  }
+}
+
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text),
+                )
+            )
+          ],
         ),
-        body: Text('Hello World'),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Friendly chat')) ,
+      body: _buildTextComposer(),
     );
   }
 }
